df63d4559ce318ae3d35c48cb065a1ba4fed117f
Alex Iudice <alex.iudice@atomrain.com>
2020-03-19 07:42:44 -0400

    HTML package.

---
+++ html/src/com/polygongames/mario/client/HtmlLauncher.java
@@ -0,0 +1,20 @@
+package com.polygongames.mario.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.iudice.GameStart;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(640, 480);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener()
+        {
+                return new GameStart();
+        }
+}
